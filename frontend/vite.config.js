3caabcd09caa78b0b42220679ea02ebd7fc2b9a9
me2sumanislam <me2sumanislam247@gmail.com>
2026-05-12 12:58:23 +0600

    fix caching

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,4 +7,13 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  }
 })
