d04a92f1e20f888cceeaefd2d079bb3e3a311382
me2sumanislam <me2sumanislam247@gmail.com>
2026-07-02 08:33:17 +0600

    login problem

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
-const APP_VERSION = "1.0.1";
+const APP_VERSION = "1.0.2";
 const STORAGE_KEY = "uthiyo_app_version";
 
 function checkAppVersion() {
